d1719b8631b2f49d2b908b8e50a1e1388a5019e7
Sebastien Marc <smarc@smarcsoft.com>
2022-09-20 10:08:50 +0200

    aws updates

--- aws/connect.ps1
+++ aws/connect.ps1
@@ -3,4 +3,5 @@ param ($username='smarcsoft', $keyfile='PMsmarcsoft.pem')
 Write-Host -NoNewline "Getting the public DNS name of the instance..."
 Invoke-Expression "aws ec2 describe-instances --region us-east-1 --query `"Reservations[].Instances[?InstanceId=='$instance_id'].PublicIpAddress[]|[0]`"" -OutVariable out | Tee-Object -Variable out 
 $dns = $out.Trim('"')
+Write-Host "Connecting with user $username and keyfile $keyfile"
 Invoke-Expression "ssh -i C:\Users\sebma\OneDrive\dev\aws\$keyfile $($username)@$($dns)"
--- aws/stop_infra.ps1
+++ aws/stop_infra.ps1
@@ -2,7 +2,16 @@
 Write-Host -NoNewline "Stopping backend main server..."
 Invoke-Expression "aws ec2 stop-instances --region $region --instance-ids $instance_id"
 Write-Host "succeeded..."
-Write-Host -NoNewline "Waiting for running status..."
+Write-Host -NoNewline "Waiting for stopped status..."
 # wait until the instance is running
 Invoke-Expression "aws ec2 wait instance-stopped --region $region --instance-ids $instance_id"
+Write-Host "instance successfully stopped"
+
+
+Write-Host -NoNewline "Stopping database server..."
+Invoke-Expression "aws ec2 stop-instances --region $region --instance-ids $db_id"
+Write-Host "succeeded..."
+Write-Host -NoNewline "Waiting for stopped status..."
+# wait until the instance is running
+Invoke-Expression "aws ec2 wait instance-stopped --region $region --instance-ids $db_id"
 Write-Host "instance successfully stopped"

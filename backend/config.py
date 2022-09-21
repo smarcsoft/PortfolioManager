7dde12f636bddaf75fd555a35296c40a10682132
Sebastien Marc <smarc@smarcsoft.com>
2022-09-21 17:46:40 +0200

    update process completed

--- backend/config.py
+++ backend/config.py
@@ -23,7 +23,7 @@ def __read_configuration(configfile:str):
         for line in configfile:
             if (len(line.strip())> 0) and (not line.strip().startswith('#')):
                 (key,value) = __parse_line(line)
-                __logger.debug("Found %s -> %s", key, value)
+                __logger.debug("Found %s -> %s", key, value.strip())
                 config[key.strip()] = value.strip()
     return config
 
@@ -32,8 +32,10 @@ def process_arguments():
     '''
     Process the command line arguments
 
-    Returns a list of exchanges to be processed.
-    En empty list means all exchanges
+    Returns a tuple of:
+        . A list of exchanges to be processed. An empty list means all exchanges
+        . The config file given as an argument
+        . An update boolean flag corresponding to the update argument specified
     '''
     config_file = "config/pm.conf"
     parser = argparse.ArgumentParser(description="Feed the database of the portfolio manager")
@@ -41,14 +43,16 @@ def process_arguments():
     parser.add_argument('--exchange', '-e', nargs='+', action="extend",type=str, help="Process the list of exchanges specified (by their codes)")
     parser.add_argument('--config', '-c', nargs='?', action="store",type=str, help="Use the configuration file specified.")
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+    parser.add_argument('--update', action="store_true", help="update only (does not process history)")
     args=parser.parse_args()
     debug_level = None
     if hasattr(args,"d"): debug_level = args.d 
     if hasattr(args,"debug"): debug_level = args.debug 
     if hasattr(args,"log"): debug_level = args.log 
     if debug_level != None:
+        logging.getLogger(None).setLevel(debug_level.upper())
         __logger.info("Debug level set to %s", debug_level.upper())
-        __logger.setLevel(debug_level.upper())
+
     if hasattr(args,"config") and (args.config != None): 
         config_file = args.config 
     exchange_list=[]
@@ -56,7 +60,10 @@ def process_arguments():
     if hasattr(args,"exchange") and (args.exchange != None): exchange_list = args.exchange 
     if len(exchange_list) != 0:
         __logger.info("Processing exchanges:%s",exchange_list)
-    return (exchange_list, config_file)
+    update = False
+    if args.update:
+        update = True
+    return (exchange_list, config_file, update)
 
 def get_config(key:str, *, configfile="config/pm.conf")->str:
     '''

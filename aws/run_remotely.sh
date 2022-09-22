024a7f3ad20e7613a6308c9e259fdd3c41c0d62f
Sebastien Marc <sebmarc@yahoo.com>
2022-09-22 12:32:27 +0000

    first remote run successfull

--- aws/run_remotely.sh
+++ aws/run_remotely.sh
@@ -4,6 +4,6 @@ $(dirname "$0")/start_infra.sh
 echo "Executing feeder..."
 #Get the public IP of the compute server
 backend_ip=$(aws ec2 describe-instances --instance-ids i-0a3774d4c3e971e64 --query Reservations[].Instances[].PublicIpAddress[] --output text)
-ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem -o StrictHostKeyChecking=no smarcsoft@$backend_ip /home/smarcsoft/dev/PortfolioManager/aws/run.sh --exchange VX
+ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem -o StrictHostKeyChecking=no smarcsoft@$backend_ip /home/smarcsoft/PortfolioManager/aws/run.sh --exchange VX
 echo "Stopping AWS infrastructure"
 $(dirname "$0")/stop_infra.sh

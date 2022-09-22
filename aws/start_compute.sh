8d86501e6a7c2f2d85f183ba7251ae475f9fcaff
Sebastien Marc <sebmarc@yahoo.com>
2022-09-22 09:45:52 +0000

    Startup shell scripts

---
+++ aws/start_compute.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+. $(dirname "$0")/vars.sh
+
+echo -n "Starting backend main server..."
+#aws ec2 start-instances --instance-ids $backend_id
+if [ $? -ne 0 ]
+then
+    echo "failed!"
+    exit -1
+fi
+echo "succeeded"
+echo -n "Waiting for running status..."
+#aws ec2 wait instance-running --instance-ids $backend_id
+if [ $? -ne 0 ]
+then
+    echo "failed!"
+    exit -1
+fi
+echo "suceeded"
---
+++ aws/vars.sh
@@ -0,0 +1,3 @@
+region="us-east-1"
+backend_id="i-0a3774d4c3e971e64"
+db_id="i-02ed02861c159f6ce"

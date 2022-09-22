5d2055867a925674421f823e9cb82dc11cfb6ea6
Sebastien Marc <sebmarc@yahoo.com>
2022-09-22 12:02:06 +0000

    public IP address of the database server to solve its connectivity from LightSail

--- aws/run_remotely.sh
+++ aws/run_remotely.sh
@@ -3,6 +3,6 @@ echo "Starting AWS infrastructure"
 $(dirname "$0")/start_infra.sh
 echo "Executing feeder..."
 . $(dirname "$0")/vars.sh #to get the IP adress of the newly started server
-echo "ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem smarcsoft@$backend_ip /home/smarcsoft/dev/PortfolioManager/aws/run.sh --exchange VX"
-#echo "Stopping AWS infrastructure"
-#$(dirname "$0")/stop_infra.sh
+ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem smarcsoft@$backend_ip /home/smarcsoft/dev/PortfolioManager/aws/run.sh --exchange VX
+echo "Stopping AWS infrastructure"
+$(dirname "$0")/stop_infra.sh

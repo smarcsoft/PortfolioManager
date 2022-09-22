f06e890516022a9a29fb34816ee33c55145a8369
Sebastien Marc <sebmarc@yahoo.com>
2022-09-22 10:01:50 +0000

    infrastructure start/stop scripts

--- aws/stop_compute.sh
+++ aws/stop_compute.sh
@@ -2,15 +2,15 @@
 . $(dirname "$0")/vars.sh
 
 echo -n "Starting backend main server..."
-aws ec2 start-instances --instance-ids $backend_id
+aws ec2 stop-instances --instance-ids $backend_id
 if [ $? -ne 0 ]
 then
     echo "failed!"
     exit -1
 fi
 echo "succeeded"
-echo -n "Waiting for running status..."
-aws ec2 wait instance-running --instance-ids $backend_id
+echo -n "Waiting for stopped status..."
+aws ec2 wait instance-stopped --instance-ids $backend_id
 if [ $? -ne 0 ]
 then
     echo "failed!"

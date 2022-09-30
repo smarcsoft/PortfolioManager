b3fd41c2300dd0941f5814a364650c5c80f5361a
Sebastien Marc <sebmarc@yahoo.com>
2022-09-30 12:57:43 +0000

    jupiter running in subdomain

---
+++ aws/run_jupiter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+NO_INFRA=0
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --no-infra)
+      NO_INFRA=1
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+
+if [ $NO_INFRA -eq 0 ]
+then
+    echo "Starting AWS compute server"
+    $(dirname "$0")/start_compute.sh
+fi
+echo "Executing jupiter on compute server ..."
+#Get the public IP of the compute server
+backend_ip=$(aws ec2 describe-instances --instance-ids i-0a3774d4c3e971e64 --query Reservations[].Instances[].PublicIpAddress[] --output text)
+ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem -o StrictHostKeyChecking=no smarcsoft@$backend_ip /home/smarcsoft/PortfolioManager/frontend/lab/run_jupiter.sh
+

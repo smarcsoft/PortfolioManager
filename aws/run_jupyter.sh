17031fb4c3dae2fa19e8d5fb80f3ab7d47529a55
Sebastien Marc <sebmarc@yahoo.com>
2022-10-19 16:05:18 +0000

    backgroun support

--- aws/run_jupyter.sh
+++ aws/run_jupyter.sh
@@ -2,11 +2,16 @@
 
 POSITIONAL_ARGS=()
 NO_INFRA=0
+BACKGROUND=
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     --no-infra)
       NO_INFRA=1
+      shift # past argument
+      ;;
+    --background)
+      BACKGROUND='--background'
       shift # past argument
       ;;
     -*|--*)
@@ -29,5 +34,5 @@ fi
 echo "Executing jupiter on compute server ..."
 #Get the public IP of the compute server
 backend_ip=$(aws ec2 describe-instances --instance-ids i-0a3774d4c3e971e64 --query Reservations[].Instances[].PublicIpAddress[] --output text)
-ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem -o StrictHostKeyChecking=no smarcsoft@$backend_ip /home/smarcsoft/PortfolioManager/frontend/lab/run_jupyter.sh
+ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem -o StrictHostKeyChecking=no smarcsoft@$backend_ip /home/smarcsoft/PortfolioManager/frontend/lab/run_jupyter.sh $BACKGROUND
 

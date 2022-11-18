54a25f42e2c1e5fb9cdf8092bcd44c7325972e5f
Sebastien Marc <sebmarc@yahoo.com>
2022-11-18 12:59:32 +0000

    fixed loaders

--- aws/run_update.sh
+++ aws/run_update.sh
@@ -4,6 +4,7 @@ POSITIONAL_ARGS=()
 NO_INFRA=0
 CONFIGFILE="config/controller.json"
 TYPE="price"
+BATCH=""
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -12,9 +13,14 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       ;;
     --type)
-	TYPE="$2" #price or fundamental_data
-	shift
-	shift
+      TYPE="$2" #price or fundamental_data
+      shift
+      shift
+      ;;
+     --batch)
+      BATCH="--batch $2"
+      shift
+      shift
       ;;
     --config)
       CONFIGFILE="$2"
@@ -41,7 +47,7 @@ fi
 echo "Executing feeder on $TYPE..."
 #Get the public IP of the compute server
 backend_ip=$(aws ec2 describe-instances --instance-ids i-0a3774d4c3e971e64 --query Reservations[].Instances[].PublicIpAddress[] --output text)
-ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem -o StrictHostKeyChecking=no -o LogLevel=quiet smarcsoft@$backend_ip /home/smarcsoft/PortfolioManager/aws/run.sh --controller --type $TYPE --config $CONFIGFILE
+ssh -i /home/smarcsoft/keys/PMsmarcsoft.pem -o StrictHostKeyChecking=no -o LogLevel=quiet smarcsoft@$backend_ip /home/smarcsoft/PortfolioManager/aws/run.sh --controller --type $TYPE --config $CONFIGFILE $BATCH
  
 if [ $NO_INFRA -eq 0 ]
 then
